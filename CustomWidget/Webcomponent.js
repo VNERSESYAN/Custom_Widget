174f90c6fa5050347bd137c405a6ecec2b3f6b19
VNERSESYAN <131447021+VNERSESYAN@users.noreply.github.com>
2023-04-21 16:33:52 +0200

    Add files via upload

---
+++ CustomWidget/Webcomponent.js
@@ -0,0 +1,31 @@
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = 
+    `<button type="button" id="myBtn">Helpetr Button</button>` ;   
+   
+    class PerformanceHelp extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
+
+        init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
+        }
+
+        fireChanged() {
+            console.log("OnClick Triggered");     
+            
+        }        
+        
+    }
+
+    customElements.define('custom-button', PerformanceHelp);
+})();
